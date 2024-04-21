edf7b078e2ea7448f9fce227a1999d52e1675c5f
Victor Monnier <117296582+Motirock@users.noreply.github.com>
2024-04-21 13:33:51 -0400

    Delete src directory

--- src/StructureType.cxx
+++
@@ -1,7 +0,0 @@
-#include "StructureType.h"
-
-StructureType::StructureType(int ID, std::string name, std::vector<std::pair<Block, glm::vec3>> blocks, 
-    std::pair<int, int> xBounds, std::pair<int, int> yBounds, std::pair<int, int> zBounds)
-    : ID(ID), name(name), blocks(blocks), xBounds(xBounds), yBounds(yBounds), zBounds(zBounds) {}
-
-StructureType::StructureType() {}
